a697bb142e0e4198c67116c4f8a3482d24a223a6
Caleb Sander <caleb.sander@gmail.com>
2016-09-27 12:56:53 -0400

    Added Vuforia license key

---
+++ TeamCode/src/main/java/org/gearticks/VuforiaKey.java
@@ -0,0 +1,5 @@
+package org.gearticks;
+
+public abstract class VuforiaKey {
+	public static final String KEY = "ATeiWz//////AAAAGRmAvkjjNUW8nBmdAgmRrdc18M66p7Z9Ber+NAD7SLRTZz1qbjNstdlATr5fgQEViVPweocCaMxIz004ykbNdIM8nUv4laKJsGeyMRhfMUBDpNqtve/AhsrcpE3Em2C2ZpGZmrlFiURJyr8qsuhwZSUre8w6ldD0SpLb9V614E1doPcES0WbHlOBLPQ5Cl9v7I+lLTBKDdlW3BCm9Mv17KfD9OcgTsUA0QAaZ2FfXpTXqUlq+Y5s5RokqwU542NMDqh6qCEkfi+HepNdgoYOv9Ln9Gs2CoYx1OHt4JScXKeVSludmWYyHAp+CfqYJZmNC3tgxilf4uyEB2OQEJpNfH8jS8BclefVRxgPWqf11Wcs";
+}
