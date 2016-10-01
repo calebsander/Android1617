ffc503bff175cf41d1b2e68efe69f7b4e8d48a48
Jack Hutchinson <JackHutchinson@Jacks-Air.fios-router.home>
2016-10-01 15:19:00 -0400

    added more functionality to teleop

--- TeamCode/src/main/java/org/gearticks/hardware/configurations/ResqConfiguration.java
+++ TeamCode/src/main/java/org/gearticks/hardware/configurations/ResqConfiguration.java
@@ -1,5 +1,7 @@
 package org.gearticks.hardware.configurations;
 
+import com.qualcomm.hardware.modernrobotics.ModernRoboticsDigitalTouchSensor;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
@@ -7,23 +9,45 @@ import org.gearticks.hardware.drive.MotorWrapper;
 import org.gearticks.hardware.drive.TankDrive;
 
 public class ResqConfiguration implements HardwareConfiguration {
-    public final DcMotor fl, fr, bl, br;
+    public final MotorWrapper screw;
+    public final MotorWrapper lift;
+    public final MotorWrapper door;
+    public final CRServo bucketLift;
+    public final MotorWrapper fl, fr, bl, br;
     public final TankDrive drive;
     public ResqConfiguration(HardwareMap hardwareMap) {
-        this.fl = (DcMotor)hardwareMap.get("fl");
-        this.fr = (DcMotor)hardwareMap.get("fr");
-        this.bl = (DcMotor)hardwareMap.get("bl");
-        this.br = (DcMotor)hardwareMap.get("br");
+        this.lift = new MotorWrapper((DcMotor)hardwareMap.get("lift"), MotorWrapper.MotorType.NEVEREST_40);
+        this.screw = new MotorWrapper((DcMotor)hardwareMap.get("screw"), MotorWrapper.MotorType.NEVEREST_20);
+        this.door = new MotorWrapper((DcMotor)hardwareMap.get("door"), MotorWrapper.MotorType.NEVEREST_40);
+        this.bucketLift = (CRServo)hardwareMap.get("bucketLift");
+        this.fl = new MotorWrapper((DcMotor)hardwareMap.get("fl"), MotorWrapper.MotorType.NEVEREST_40);
+        this.fr = new MotorWrapper((DcMotor)hardwareMap.get("fr"), MotorWrapper.MotorType.NEVEREST_40);
+        this.bl = new MotorWrapper((DcMotor)hardwareMap.get("bl"), MotorWrapper.MotorType.NEVEREST_40);
+        this.br = new MotorWrapper((DcMotor)hardwareMap.get("br"), MotorWrapper.MotorType.NEVEREST_40);
         this.drive = new TankDrive();
-        this.drive.addLeftMotor(new MotorWrapper(this.fl, MotorWrapper.MotorType.NEVEREST_40));
-        this.drive.addLeftMotor(new MotorWrapper(this.bl, MotorWrapper.MotorType.NEVEREST_40));
-        this.drive.addRightMotor(new MotorWrapper(this.fr, MotorWrapper.MotorType.NEVEREST_40));
-        this.drive.addRightMotor(new MotorWrapper(this.br, MotorWrapper.MotorType.NEVEREST_40));
+        this.drive.addLeftMotor(this.fl);
+        this.drive.addLeftMotor(this.bl);
+        this.drive.addRightMotor(this.fr);
+        this.drive.addRightMotor(this.br);
     }
     public void teardown() {
 
     }
     public void stopMotion() {
 
+    }
+
+    public static abstract class MotorConstants {
+        public static final double SCREW_BACKWARD = 1.0;
+        public static final double SCREW_FORWARD = -SCREW_BACKWARD;
+
+        public static final double DOOR_LEFT = 1.0;
+        public static final double DOOR_RIGHT = -DOOR_LEFT;
+
+        public static final double BUCKET_UP = 1.0;
+        public static final double BUCKET_DOWN = -BUCKET_UP;
+
+        public static final double LIFT_UP = 1.0;
+        public static final double LIFT_DOWN = -LIFT_UP;
     }
 }
