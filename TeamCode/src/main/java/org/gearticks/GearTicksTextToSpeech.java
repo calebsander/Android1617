d544d0b014f14ef809737811bcb1e3e771ffd5fa
Caleb Sander <caleb.sander@gmail.com>
2016-10-07 20:40:54 -0400

    Added text-to-speech capability; Range Test config

---
+++ TeamCode/src/main/java/org/gearticks/GearTicksTextToSpeech.java
@@ -0,0 +1,30 @@
+//Manages text-to-speech
+package org.gearticks;
+
+import android.content.Context;
+import android.speech.tts.TextToSpeech;
+import java.util.Locale;
+
+public class GearTicksTextToSpeech {
+	private final TextToSpeech tts;
+	private boolean ready;
+
+	public GearTicksTextToSpeech(Context context) {
+		this.tts = new TextToSpeech(context, new TextToSpeech.OnInitListener() {
+			public void onInit(int status) {
+				GearTicksTextToSpeech.this.ready = true;
+			}
+		});
+		this.tts.setLanguage(Locale.US);
+		this.ready = false;
+	}
+
+	//Queues the text to be spoken
+	public void speak(String text) {
+		this.tts.speak(text, TextToSpeech.QUEUE_ADD, null);
+	}
+	//Returns whether it is ready (seems to return true a bit prematurely)
+	public boolean isReady() {
+		return this.ready;
+	}
+}
---
+++ TeamCode/src/main/java/org/gearticks/opmodes/test/TextToSpeechTest.java
@@ -0,0 +1,46 @@
+package org.gearticks.opmodes.test;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import org.gearticks.GearTicksTextToSpeech;
+import org.gearticks.opmodes.BaseOpMode;
+
+@Autonomous(name = "TTS", group = "test")
+@Disabled
+public class TextToSpeechTest extends BaseOpMode {
+	private static final String[] WORDS = new String[]{
+		"gear",
+		"ticks",
+		"dingus",
+		"Schrodingus",
+		"sneaky beaky",
+		"we are not sexist on this team",
+		"optimized",
+		"Seize the tail of youth!",
+		"manget",
+		"contribooture",
+		"sauce me the wrench, gucci"
+	};
+	private static final double DELAY_SECONDS = 1.5;
+
+	private int wordIndex;
+	private ElapsedTime wordTimer;
+	private GearTicksTextToSpeech tts;
+
+	protected void matchStart() {
+		this.wordIndex = 0;
+		this.wordTimer = new ElapsedTime();
+		this.tts = new GearTicksTextToSpeech(this.hardwareMap.appContext);
+	}
+	protected void loopAfterStart() {
+		final boolean ready = this.tts.isReady();
+		this.telemetry.addData("Ready", ready);
+		this.telemetry.update();
+		if (ready && this.wordTimer.seconds() > DELAY_SECONDS) {
+			this.tts.speak(WORDS[wordIndex]);
+			this.wordIndex = (this.wordIndex + 1) % WORDS.length;
+			this.wordTimer.reset();
+		}
+	}
+}
