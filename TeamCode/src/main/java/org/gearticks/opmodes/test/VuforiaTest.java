061af5c1aa7b85bb5ad03a7682c5475d2f3c54a9
Caleb Sander <caleb.sander@gmail.com>
2016-11-11 19:11:46 -0500

    Can choose camera direction in VuforiaTest

--- TeamCode/src/main/java/org/gearticks/opmodes/test/VuforiaTest.java
+++ TeamCode/src/main/java/org/gearticks/opmodes/test/VuforiaTest.java
@@ -8,27 +8,33 @@ import com.vuforia.Vuforia;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import org.gearticks.VuforiaKey;
+import org.gearticks.joystickoptions.ValuesJoystickOption;
 import org.gearticks.opmodes.BaseOpMode;
 
-@Autonomous(name = "Vuforia Test", group = "test")
+@Autonomous
 @Disabled
 public class VuforiaTest extends BaseOpMode {
 	private static final String[] IMAGE_NAMES = new String[]{"Wheels", "Tools", "Legos", "Gears"};
 	private VuforiaTrackables beaconImages;
+	private ValuesJoystickOption<CameraDirection> frontOption;
 
 	public void initialize() {
+		this.frontOption = new ValuesJoystickOption<>("Camera", CameraDirection.values());
+		this.addOption(this.frontOption);
+	}
+	public void matchStart() {
 		final VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
 		parameters.vuforiaLicenseKey = VuforiaKey.KEY;
+		parameters.cameraDirection = this.frontOption.getRawSelectedOption();
 		final VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(parameters);
 		Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
 		this.beaconImages = vuforia.loadTrackablesFromAsset("FTC_2016-17");
 		for (int i = 0; i < IMAGE_NAMES.length; i++) this.beaconImages.get(i).setName(IMAGE_NAMES[i]);
-	}
-	public void matchStart() {
 		this.beaconImages.activate();
 	}
 	public void loopAfterStart() {
