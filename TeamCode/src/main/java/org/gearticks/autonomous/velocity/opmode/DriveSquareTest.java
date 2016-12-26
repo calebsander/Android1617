49f777c1a5bb3a70c53f1134a18cc75a011c07a9
ireneterpstra <ireneterpstra2@gmail.com>
2016-12-26 17:09:55 -0500

    Drive Square Test v2

--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/DriveSquareTest.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/DriveSquareTest.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by vterpstra on 12/26/2016.
+ *
  */
 @Autonomous(name = "Color and Gyro Sensor Test Autonomous")
 public class DriveSquareTest extends VelocityBaseOpMode {
@@ -20,7 +20,8 @@ public class DriveSquareTest extends VelocityBaseOpMode {
 
     protected void initialize() {
         super.initialize();
-        this.sm.setup();
+        this.createComponents();
+        this.sm.initialize();
     }
 
     protected void loopBeforeStart() {
@@ -29,6 +30,7 @@ public class DriveSquareTest extends VelocityBaseOpMode {
 
     protected void matchStart() {
         super.matchStart();
+        this.sm.setup();
     }
 
     protected void loopAfterStart(){
@@ -38,13 +40,16 @@ public class DriveSquareTest extends VelocityBaseOpMode {
 
     protected void matchEnd() {
         super.matchEnd();
+        this.sm.tearDown();
     }
 
     protected void createComponents(){
         List<AutonomousComponent> components = new ArrayList<>();
 //        components.add(new Wait(2000, null, "Wait for 2 sec"));
-//        components.add(new GiroDriveEncoder(2000, 100, 2000, null, "Drive for 2 sec heading 100"));
-//        components.add(new Wait(2000, null, "Wait for 2 sec"));
+        components.add(new GiroDriveEncoder(0.0, 0.5, 2000, null, "Drive for 2000 ticks heading forward"));
+        components.add(new Wait(2000, null, "Wait for 2 sec"));
+        components.add(new GiroDriveEncoder(0.0, 0.5, 2000, null, "Drive for 2000 ticks heading forward"));
+        components.add(new Wait(2000, null, "Wait for 2 sec"));
         sm = new LinearStateMachineFullImpl(components);
     }
 }
