051c27ecd42ead0e35088457b4c6c4ba3ec382ea
Victor Terpstra <vjterpstra@gmail.com>
2016-12-26 16:41:04 -0500

    DriveSquareTest first version

--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/DriveSquareTest.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/DriveSquareTest.java
@@ -1,16 +1,26 @@
 package org.gearticks.autonomous.velocity.opmode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.gearticks.autonomous.generic.component.AutonomousComponent;
 import org.gearticks.autonomous.generic.opmode.VelocityBaseOpMode;
+import org.gearticks.autonomous.generic.statemachine.network.LinearStateMachineFullImpl;
+import org.gearticks.autonomous.velocity.components.GiroDriveEncoder;
+import org.gearticks.autonomous.velocity.components.Wait;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by vterpstra on 12/26/2016.
  */
-
+@Autonomous(name = "Color and Gyro Sensor Test Autonomous")
 public class DriveSquareTest extends VelocityBaseOpMode {
-
+    private LinearStateMachineFullImpl sm;
 
     protected void initialize() {
         super.initialize();
+        this.sm.setup();
     }
 
     protected void loopBeforeStart() {
@@ -21,7 +31,20 @@ public class DriveSquareTest extends VelocityBaseOpMode {
         super.matchStart();
     }
 
+    protected void loopAfterStart(){
+        super.loopAfterStart();
+        this.sm.run();
+    }
+
     protected void matchEnd() {
         super.matchEnd();
+    }
+
+    protected void createComponents(){
+        List<AutonomousComponent> components = new ArrayList<>();
+//        components.add(new Wait(2000, null, "Wait for 2 sec"));
+//        components.add(new GiroDriveEncoder(2000, 100, 2000, null, "Drive for 2 sec heading 100"));
+//        components.add(new Wait(2000, null, "Wait for 2 sec"));
+        sm = new LinearStateMachineFullImpl(components);
     }
 }
--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/SampleAutonomousOpMode.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/SampleAutonomousOpMode.java
@@ -1,26 +1,26 @@
 package org.gearticks.autonomous.velocity.opmode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import org.gearticks.autonomous.generic.opmode.VelocityBaseOpMode;
 
-/**
- * Created by vterpstra on 12/26/2016.
- */
-
+@Autonomous(name = "Sample Autonomous")
+@Disabled
 public class SampleAutonomousOpMode extends VelocityBaseOpMode {
 
 
     protected void initialize() {
         super.initialize();
     }
-
     protected void loopBeforeStart() {
         super.loopBeforeStart();
     }
-
     protected void matchStart() {
         super.matchStart();
     }
-
+    protected void loopAfterStart(){
+        super.loopAfterStart();
+    }
     protected void matchEnd() {
         super.matchEnd();
     }
