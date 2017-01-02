b9415708c5a5d91ccc1310d21ff4f52ad7e8c108
Caleb Sander <caleb.sander@gmail.com>
2017-01-01 19:54:55 -0500

    No longer need to wait for color sensor to turn on

--- TeamCode/src/main/java/org/gearticks/opmodes/test/TCS34725Test.java
+++ TeamCode/src/main/java/org/gearticks/opmodes/test/TCS34725Test.java
@@ -1,32 +1,29 @@
 package org.gearticks.opmodes.test;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.I2cDevice;
 import com.qualcomm.robotcore.hardware.LED;
 import org.gearticks.dimsensors.i2c.TCS34725;
+import org.gearticks.opmodes.BaseOpMode;
 
-@Autonomous
-public class TCS34725Test extends OpMode {
+@TeleOp
+public class TCS34725Test extends BaseOpMode {
 	private TCS34725 colorSensor;
 	private LED led;
 
-	public void init() {
+	public void initialize() {
 		this.colorSensor = new TCS34725((I2cDevice)this.hardwareMap.get("whiteLineColor"));
+		this.colorSensor.startReadingColor();
 		this.led = (LED)this.hardwareMap.get("whiteLineColorLed");
 	}
-	public void start() {
-		this.colorSensor.startReadingColor();
-		this.led.enable(true);
-	}
-	public void loop() {
+	public void loopAfterStart() {
 		this.led.enable(this.gamepad1.a);
 		this.telemetry.addData("Clear", this.colorSensor.getClear());
 		this.telemetry.addData("Red", this.colorSensor.getRed());
 		this.telemetry.addData("Green", this.colorSensor.getGreen());
 		this.telemetry.addData("Blue", this.colorSensor.getBlue());
 	}
-	public void stop() {
+	public void matchEnd() {
 		this.colorSensor.stopReading();
 	}
 }
