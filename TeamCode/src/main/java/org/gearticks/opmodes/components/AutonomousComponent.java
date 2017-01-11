e18add48a8117a40d16a4198fe0053bb005784bd
Caleb Sander <caleb.sander@gmail.com>
2017-01-10 23:35:46 -0500

    Revert "Merge remote-tracking branch 'refs/remotes/origin/feature/caleb-auton-components'"
    This reverts commit 0470784ec6591731e6c785adc965ebc3eaf21429, reversing
    changes made to 870da2e141ea014d775891160ca84e8496bbc2c6.

--- TeamCode/src/main/java/org/gearticks/opmodes/components/AutonomousComponent.java
+++
@@ -1,9 +0,0 @@
-package org.gearticks.opmodes.components;
-
-public interface AutonomousComponent<OP_MODE_CLASS> {
-	void init(OP_MODE_CLASS opMode);
-	void loop(OP_MODE_CLASS opMode);
-	boolean isDone(OP_MODE_CLASS opMode);
-	AutonomousComponent<OP_MODE_CLASS> getNextComponent(OP_MODE_CLASS opMode);
-	void stop(OP_MODE_CLASS opMode);
-}
