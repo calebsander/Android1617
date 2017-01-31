9d5ec0dea8a920749a60263f3ffd9f1361291ea0
Victor Terpstra <vjterpstra@gmail.com>
2017-01-31 18:52:04 -0500

    Added code from ElectronVolts
    See how we can best use this

---
+++ TeamCode/src/main/java/org/gearticks/opencv/vision/BeaconProcessorTemplate.java
@@ -0,0 +1,18 @@
+package org.gearticks.opencv.vision;
+
+
+import org.opencv.core.Mat;
+
+/**
+ *
+ * FTC Team EV 7393
+ */
+public class BeaconProcessorTemplate implements ImageProcessor<BeaconColorResult> {
+    
+    @Override
+    public ImageProcessorResult<BeaconColorResult> process(long startTime, Mat rgbaFrame, boolean saveImages) {
+        
+        //<The code we write will go here>
+        return null;
+    }
+}
