057474cc53350b1cb95fc30a758e99b0cacbb290
D01PH <dolfinrule@gmail.com>
2017-02-05 15:54:24 -0500

    Added cap ball to red side, added disengage rollers component

--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/components/velocity/composite/RedSideAutonomous.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/components/velocity/composite/RedSideAutonomous.java
@@ -8,11 +8,13 @@ import org.gearticks.autonomous.generic.statemachine.LinearStateMachine;
 import org.gearticks.autonomous.velocity.components.experimental.GiroBananaTurnEncoder;
 import org.gearticks.autonomous.velocity.components.experimental.GiroDriveAlongWallEncoder;
 import org.gearticks.autonomous.velocity.components.experimental.GiroDriveAlongWallLine;
+import org.gearticks.autonomous.velocity.components.generic.DebugPause;
 import org.gearticks.autonomous.velocity.components.generic.GiroDriveEncoder;
 import org.gearticks.autonomous.velocity.components.generic.GiroDriveEncoderNoStop;
 import org.gearticks.autonomous.velocity.components.generic.GiroDriveToLine;
 import org.gearticks.autonomous.velocity.components.generic.GiroTurn;
 import org.gearticks.autonomous.velocity.components.generic.Wait;
+import org.gearticks.autonomous.velocity.components.velocity.single.DisengageSideRollers;
 import org.gearticks.hardware.configurations.VelocityConfiguration;
 import org.gearticks.vuforia.VuforiaConfiguration;
 
@@ -40,5 +42,11 @@ public class RedSideAutonomous extends LinearStateMachine {
         addComponent(new GiroDriveToLine(0, 0.05, 500, configuration, "Adjust to white line"));
         addComponent(new GiroTurn(0.0, configuration, "Straighten out"));
         addComponent(new SidePressBeaconButton(vuforiaConfiguration, configuration, "Press Button"));
+
+        //Cap ball
+        //addComponent(new DebugPause(gamepads, telemetry ,configuration, "Press A to continue"));
+        addComponent(new DisengageSideRollers(configuration, "Disengage rollers"));
+        addComponent(new GiroBananaTurnEncoder(0.0, -135.0, 0.7, 3500, configuration, "Banana to face Cap Ball"));
+        addComponent(new GiroDriveEncoder(-135.0, 0.7, 5500, configuration, "Hit and park"));
     }
 }
