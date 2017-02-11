11ee939dc50da73cddd107bb7ca9fc933cff5f5c
Caleb Sander <caleb.sander@gmail.com>
2017-02-11 10:14:24 -0500

    Added straighten out on blue side before beacons

--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/components/velocity/composite/BlueSideAutonomous.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/components/velocity/composite/BlueSideAutonomous.java
@@ -30,22 +30,22 @@ public class BlueSideAutonomous extends LinearStateMachine {
         addComponent(new GiroTurn(180.0, 0.1, 20, configuration, "Straighten out"));
         addComponent(new DeploySideRollers(configuration, "Deploy rollers"));
         //addComponent(new GiroDriveAlongWallEncoder(distanceFromWall, 180.0, -0.4, 2000, configuration, "Drive backwards"));
-        addComponent(new GiroDriveAlongWallLine(distanceFromWall-1, 180.0, 0.15, 6000, configuration, "Drive to beacon"));
+        addComponent(new GiroDriveAlongWallLine(distanceFromWall - 1, 180.0, 0.2, 6000, configuration, "Drive to beacon"));
 
         //Press beacon
         addComponent(new GiroDriveAlongWallLine(distanceFromWall, 180.0, -0.17, 500, configuration, "Adjust to white line"));
         addComponent(new GiroDriveAlongWallEncoder(distanceFromWall, 180.0, -0.17, 25, configuration, "Align"));
-	    //addComponent(new DebugPause(gamepads, telemetry, configuration, "pause"));
+        addComponent(new GiroTurn(180.0, configuration, "Straighten out"));
         addComponent(new SidePressBeaconButton(vuforiaConfiguration, configuration, "Press Button"));
 
         //Go to second beacon
         addComponent(new GiroDriveAlongWallEncoder(distanceFromWall, 180.0, -0.8, 3000, configuration, "Range sensor drive along wall"));
-        addComponent(new GiroDriveAlongWallLine(distanceFromWall+1, 180.0, -0.25, 2000, configuration, "Range sensor drive along wall to line"));
+        addComponent(new GiroDriveAlongWallLine(distanceFromWall + 1, 180.0, -0.25, 2000, configuration, "Range sensor drive along wall to line"));
         //sm.addComponent(new GiroDriveToLine(180, 0.7, 8000, this.configuration, "Drive to white line"));
 
         //Press second beacon
         addComponent(new GiroDriveToLine(180, 0.05, 500, configuration, "Adjust to white line"));
-	    //addComponent(new DebugPause(gamepads, telemetry, configuration, "pause"));
+        addComponent(new GiroTurn(180.0, configuration, "Straighten out"));
         addComponent(new SidePressBeaconButton(vuforiaConfiguration, configuration, "Press Button"));
 
         //Cap ball
