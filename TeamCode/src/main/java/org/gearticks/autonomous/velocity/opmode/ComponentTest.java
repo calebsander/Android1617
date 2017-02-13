c00892bbd1d74b31dbfb98f913eba81feb515979
Caleb Sander <caleb.sander@gmail.com>
2017-02-13 17:05:38 -0500

    Cleared ComponentTest

--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/ComponentTest.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/ComponentTest.java
@@ -3,21 +3,8 @@ package org.gearticks.autonomous.velocity.opmode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.gearticks.autonomous.generic.OpModeContext;
 import org.gearticks.autonomous.generic.statemachine.LinearStateMachine;
-import org.gearticks.autonomous.velocity.components.experimental.GiroDriveAlongWallEncoder;
-import org.gearticks.autonomous.velocity.components.experimental.GiroDriveAlongWallLine;
-import org.gearticks.autonomous.velocity.components.generic.DebugPause;
-import org.gearticks.autonomous.velocity.components.generic.GiroDriveEncoder;
-import org.gearticks.autonomous.velocity.components.generic.GiroDriveToLine;
-import org.gearticks.autonomous.velocity.components.generic.GiroTurn;
-import org.gearticks.autonomous.velocity.components.velocity.composite.Shoot2Balls;
-import org.gearticks.autonomous.velocity.components.velocity.composite.SidePressBeaconButton;
-import org.gearticks.autonomous.velocity.components.velocity.single.DisengageBeaconServo;
-import org.gearticks.autonomous.velocity.components.velocity.single.LeftPressBeaconServo;
-import org.gearticks.autonomous.velocity.components.velocity.single.ResetSnake;
-import org.gearticks.autonomous.velocity.components.velocity.single.RightPressBeaconServo;
 import org.gearticks.autonomous.velocity.opmode.generic.VelocityBaseOpMode;
 import org.gearticks.hardware.configurations.VelocityConfiguration;
-import org.gearticks.vuforia.VuforiaConfiguration;
 import org.gearticks.autonomous.generic.component.AutonomousComponent;
 
 @Autonomous
@@ -25,7 +12,7 @@ public class ComponentTest extends VelocityBaseOpMode {
     protected AutonomousComponent getComponent(OpModeContext<VelocityConfiguration> opModeContext) {
         final LinearStateMachine sm = new LinearStateMachine();
 
-        sm.addComponent(new GiroDriveAlongWallEncoder(10.0, 0.0, 0.05, 10000, opModeContext, "Press Button"));
+        //Add components to sm here
 
         return sm;
     }
