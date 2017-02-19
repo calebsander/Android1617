0a415edf1ff1e56a60122f069a980f6fb9adcbbc
Caleb Sander <caleb.sander@gmail.com>
2017-02-19 16:42:35 -0500

    Cleared ComponentTest

--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/ComponentTest.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/ComponentTest.java
@@ -3,9 +3,6 @@ package org.gearticks.autonomous.velocity.opmode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import org.gearticks.autonomous.generic.OpModeContext;
 import org.gearticks.autonomous.generic.statemachine.LinearStateMachine;
-import org.gearticks.autonomous.velocity.components.velocity.composite.Shoot3Balls;
-import org.gearticks.autonomous.velocity.components.velocity.single.MoveShooterDown;
-import org.gearticks.autonomous.velocity.components.velocity.single.RunIntake;
 import org.gearticks.autonomous.velocity.opmode.generic.VelocityBaseOpMode;
 import org.gearticks.hardware.configurations.VelocityConfiguration;
 import org.gearticks.autonomous.generic.component.AutonomousComponent;
@@ -15,8 +12,7 @@ public class ComponentTest extends VelocityBaseOpMode {
     protected AutonomousComponent getComponent(OpModeContext<VelocityConfiguration> opModeContext) {
         final LinearStateMachine sm = new LinearStateMachine();
 
-        sm.addComponent(new MoveShooterDown(opModeContext, "Move shooter down"));
-        sm.addComponent(new Shoot3Balls(opModeContext, "Shoot 3 balls"));
+        //Add components here
 
         return sm;
     }
