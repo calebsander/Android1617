2857487de09b19d9541a0b679ba03b03cbe6b476
calvinterpstra <calvinterpstra@gmail.com>
2017-02-25 21:26:47 -0500

    work on red side autonomous

--- TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/RedBeaconFirstAutonomous.java
+++ TeamCode/src/main/java/org/gearticks/autonomous/velocity/opmode/RedBeaconFirstAutonomous.java
@@ -47,21 +47,23 @@ public class RedBeaconFirstAutonomous extends VelocityBaseOpMode {
     protected AutonomousComponent getComponent(OpModeContext<VelocityConfiguration> opModeContext) {
         final LinearStateMachine sm = new LinearStateMachine();
 
-        final LinearStateMachine waitAndDrive = new LinearStateMachine();
+        final LinearStateMachine driveAndBTurn = new LinearStateMachine();
         //waitAndDrive.addComponent(new Wait(0.5, "Wait for 0.5"));
-        waitAndDrive.addComponent(new GiroDriveEncoderNoStop(315.0, 0.6, 4000, opModeContext, "Drive forward"));
-        waitAndDrive.addComponent(new BananaTurnNoGiro(0.0, 0.6, 6000, opModeContext, "Banana turn to wall"));
+        driveAndBTurn.addComponent(new GiroDriveEncoderNoStop(315.0, 0.5, 4000, opModeContext, "Drive forward"));
+        //waitAndDrive.addComponent(new BananaTurnNoGiro(0.0, 0.6, 5000, opModeContext, "Banana turn to wall"));
+        driveAndBTurn.addComponent(new GiroBananaTurnEncoder(-45.0, 0.0, 0.9, 6000, 0.18, 0.25, 3.0, opModeContext, "Banana turn to 0.0"));
         final LinearStateMachine rollers = new LinearStateMachine();
         rollers.addComponent(new Wait(2.0, "Wait for 2 secs"));
         rollers.addComponent(new DeploySideRollers(opModeContext, "Deploy side rollers"));
         final ParallelComponent intakeAndDrive = new ParallelComponent();
-        intakeAndDrive.addComponent(new RunIntake(4.0, true, opModeContext, "Intake particle"));
+        intakeAndDrive.addComponent(new RunIntake(4.5, true, opModeContext, "Intake particle"));
         intakeAndDrive.addComponent(rollers);
-        intakeAndDrive.addComponent(waitAndDrive);
+        intakeAndDrive.addComponent(driveAndBTurn);
 
         //Get to far beacon
         sm.addComponent(intakeAndDrive);
-        sm.addComponent(new GiroDriveAlongWallLine(DISTANCE_FROM_WALL, 0.0, 0.3, 4000, opModeContext, "Get to far beacon"));
+	    //sm.addComponent(new DebugPause(opModeContext));
+	    sm.addComponent(new GiroDriveAlongWallLine(DISTANCE_FROM_WALL, 0.0, 0.2, 4000, opModeContext, "Get to far beacon"));
 
         //Press beacon
         sm.addComponent(new GiroDriveAlongWallLine(DISTANCE_FROM_WALL, 0.0, -0.17, 500, opModeContext, "Adjust to white line"));
@@ -69,18 +71,18 @@ public class RedBeaconFirstAutonomous extends VelocityBaseOpMode {
         sm.addComponent(new SidePressBeaconButton(false, opModeContext, "Press Button"));
 
         //Get to near beacon
-        sm.addComponent(new GiroDriveAlongWallEncoder(DISTANCE_FROM_WALL, 0.0, -0.4, 3000, opModeContext, "Range sensor drive along wall"));
-        sm.addComponent(new GiroDriveAlongWallLine(DISTANCE_FROM_WALL + 1, 0.0, -0.3, 2000, opModeContext, "Range sensor drive along wall to line"));
+        sm.addComponent(new GiroDriveAlongWallEncoder(DISTANCE_FROM_WALL, 0.0, -0.8, 3000, opModeContext, "Range sensor drive along wall"));
+        sm.addComponent(new GiroDriveAlongWallLine(DISTANCE_FROM_WALL, 0.0, -0.2, 3000, opModeContext, "Get to near beacon"));
 
         //Press beacon
-        sm.addComponent(new GiroDriveAlongWallLine(DISTANCE_FROM_WALL, 0.0, 0.17, 500, opModeContext, "Adjust to white line"));
+        sm.addComponent(new GiroDriveAlongWallLine(DISTANCE_FROM_WALL, 0.0, 0.075, 500, opModeContext, "Adjust to white line"));
         sm.addComponent(new GiroTurn(0.0, opModeContext, "Straighten out"));
         sm.addComponent(new SidePressBeaconButton(false, opModeContext, "Press Button"));
 
         //Shoot
         sm.addComponent(new RaiseSideRollers(opModeContext, "Raise rollers"));
-        sm.addComponent(new GiroBananaTurnEncoder(0.0, 7.0, -0.4, 400, opModeContext, "Banana turn to -7"));
-        sm.addComponent(new GiroBananaTurnEncoder(7, 90.0, 0.5, 500, 0.1, 0.2, 1.0, opModeContext, "banana turn to shoot"));
+        sm.addComponent(new GiroBananaTurnEncoder(0.0, 9.0, -0.4, 400, opModeContext, "Banana turn to 9"));
+        sm.addComponent(new GiroBananaTurnEncoder(9, 90.0, 0.5, 500, 0.1, 0.2, 1.0, opModeContext, "banana turn to shoot"));
         sm.addComponent(new Shoot3Balls(true, opModeContext, "Shoot"));
 
         //Cap ball
